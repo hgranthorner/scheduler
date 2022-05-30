173f417a767f37d6cbe56270caa0544842b5d28e
Grant <hgranthorner@gmail.com>
2022-05-29 21:07:06 -0400

    can view activities

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,19 +16,30 @@ data Activity
 
 executeCommand :: [String] -> IO ()
 executeCommand ["add", activityName, activityDueDate] = do
-  activities <- readActivities
+  activities <- maybeReadActivities
   case maybeParseUTCTime "%Y-%m-%d %H:%M" activityDueDate of
     Just date -> writeToCache $ DueActivity {name = activityName, dueDate = date} : fromJust activities
     Nothing -> putStrLn "Failed to parse date"
+
 executeCommand ["add", activityName, activityStartDate, activityEndDate] = do
-  activities <- readActivities
+  activities <- maybeReadActivities
   case (maybeParseUTCTime "%Y-%m-%d %H:%M" activityStartDate, maybeParseUTCTime "%Y-%m-%d" activityEndDate) of
     (Just start, Just end) -> writeToCache $ SpanActivity {name = activityName, startDate = start, endDate = end} : fromJust activities
     _ -> putStrLn "Failed to parse date"
+
+executeCommand ["get"] = do
+  maybeActivities <- maybeReadActivities
+  case maybeActivities of
+    Just activities -> mapM_ (putStrLn . displayActivity) activities
+    Nothing -> putStrLn "Failed to read activities"
+
+executeCommand ["create-cache"] = do
+  writeFile "cache.txt" "[]"
+
 executeCommand _ = putStrLn "Unknown command"
 
-readActivities :: IO (Maybe [Activity])
-readActivities = do
+maybeReadActivities :: IO (Maybe [Activity])
+maybeReadActivities = do
   contents <- TIO.readFile "cache.txt"
   return (readMaybe (T.unpack contents) :: Maybe [Activity])
 
@@ -37,3 +48,19 @@ writeToCache = writeFile "cache.txt" . show
 
 maybeParseUTCTime :: String -> String -> Maybe UTCTime
 maybeParseUTCTime format str = parseTimeM True defaultTimeLocale format str :: Maybe UTCTime
+
+displayActivity :: Activity -> String
+displayActivity a =
+  case a of
+    DueActivity {name=n, dueDate=d} ->
+      "Activity: " ++ n ++ "\n" ++
+      "Due: " ++ show d ++ "\n"
+      
+    SpanActivity {name=n, startDate=s, endDate=e} ->
+      "Activity: " ++ n ++ "\n" ++
+      "Start: " ++ show s ++ "\n" ++
+      "end: " ++ show e ++ "\n"
+
+
+throwAway :: a -> ()
+throwAway _ = ()
